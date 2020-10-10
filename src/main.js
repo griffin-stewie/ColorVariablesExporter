2fa8f0ee7bddbfc2c48299bc3188439f005aa060
griffin-stewie <panterathefamilyguy@gmail.com>
2020-10-10 11:23:51 +0900

    add ColorsXMLExporter

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
-import { ColorSetExporter } from './ColorSetExporter'
+import { ColorSetExporter } from './Exporters/ColorSetExporter'
+import { ColorsXMLExporter } from './Exporters/ColorsXMLExporter'
 
 export function exportAsColorSet(context) {
   const doc = sketch.getSelectedDocument()
   const swatches = doc.swatches
   const exporter = new ColorSetExporter(swatches, doc.colorSpace)
+
+  const manager = NSFileManager.defaultManager()
+  const desktopURL = manager.URLsForDirectory_inDomains(NSDesktopDirectory, NSUserDomainMask)[0]
+
+  exporter.exportAsFile(desktopURL)
+
+  sketch.UI.message(`Exported at ${desktopURL} 🙌`)
+}
+
+export function exportAsColorXML(context) {
+  const doc = sketch.getSelectedDocument()
+  const swatches = doc.swatches
+  const exporter = new ColorsXMLExporter(swatches, doc.colorSpace)
 
   const manager = NSFileManager.defaultManager()
   const desktopURL = manager.URLsForDirectory_inDomains(NSDesktopDirectory, NSUserDomainMask)[0]
